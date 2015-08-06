bab31d51e8a0e994110a554cc36ecd1e77741f86
w3cay <w3cay@foxmail.com>
2015-08-06 11:41:15 +0800

    mod

---
+++ 轮播图/remove.js
@@ -0,0 +1,10 @@
+<script defer="defer">
+ $(document).ready(function () {
+	var deleteNode = document.getElementsByTagName("div")[183];
+	deleteNode.style.display = "block !important";
+	// document.body.removeChild(deleteNode);
+	// console.log(deleteNode.innerText);
+});
+	
+	
+</script>
